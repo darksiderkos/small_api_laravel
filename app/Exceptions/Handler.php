7dc9142d5669c931cac32ccd2f8049ef095800d2
Kos Kicheglov <k.kicheglov@gmail.com>
2017-03-21 23:40:28 +0200

    arrgghh

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,6 +45,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof HttpException){
+            return response()->json([
+                'error' => [
+                    'http_code' => 404,
+                    'message' => 'Such route does not exist'
+                ]
+            ]);
+        }
         return parent::render($request, $exception);
     }
 
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,17 +16,27 @@ class ProductController extends ApiController
     public function index($categoryId)
     {
         $eagerLoad = \Request::get('include');
+
         if ($eagerLoad) {
             $products = Product::with($eagerLoad)->where('category_id', '=', $categoryId)->get();
+            if ($products->isEmpty()) {
+                return $this->errorNotFound();
+            }
             return $this->respondWithCollection($products, new ProductTransformer());
+
+
         } else {
             $products = Product::where('category_id', '=', $categoryId)->get();
+            if ($products->isEmpty()) {
+                return $this->errorNotFound();
+            }
             return $this->respondWithCollection($products, new ProductTransformer());
         }
 
     }
 
-    public function show($productId)
+    public
+    function show($productId)
     {
         $arr = explode(',', $productId); //can request few products
         $product = Product::find($arr);
